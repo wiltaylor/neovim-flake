abebf495f52a22db7b53a168d860306b879456ef
Wil Taylor <cert@wiltaylor.dev>
2022-01-26 15:21:08 +1000

    more fixes

--- flake.nix
+++ flake.nix
@@ -158,7 +158,7 @@
     defaultPackage = lib.withDefaultSystems (sys: self.packages."${sys}".neovimWT);
 
     overlay = top: last: {
-      neovimWT = lib.neovimBuilder last;
+      neovimWT = lib.neovimBuilder allPkgs."${top.system}";
       neovimPlugins = allPkgs."${top.system}".neovimPlugins;
     };
 
