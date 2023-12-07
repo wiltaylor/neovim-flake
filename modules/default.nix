f9d891fa1a6812b84b036cafa8f56cfbc03cb7fa
Micah N Gorrell <m@minego.net>
2023-12-07 07:56:52 -0700

    Speeling

--- modules/default.nix
+++ modules/default.nix
@@ -1,17 +1,17 @@
 {config, lib, pkgs, ...}:
 {
-  imports = [
-    ./core
-    ./basic
-    ./themes
-    ./dashboard
-    ./statusline
-    ./lsp
-    ./fuzzyfind
-    ./filetree
-    ./git
-    ./formating
-    ./editor
-    ./test
-  ];
+	imports = [
+		./core
+		./basic
+		./themes
+		./dashboard
+		./statusline
+		./lsp
+		./fuzzyfind
+		./filetree
+		./git
+		./formatting
+		./editor
+		./test
+	];
 }
